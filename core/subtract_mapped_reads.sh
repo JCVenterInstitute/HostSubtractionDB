b91000b3c86d15d31484948b6f89169c89fe5e76
JasonRafeMiller <jmiller@jcvi.org>
2017-07-18 19:50:01 -0400

    Invoke function directly rather than using bash function wrapper, which gave 'file not found' errors.

--- core/subtract_mapped_reads.sh
+++ core/subtract_mapped_reads.sh
@@ -27,15 +27,6 @@ echo "GET READS FROM ${IN_R1} AND ${IN_R2}"
 echo "GET MAPS FROM ${IN_BAM1} AND ${IN_BAM2}"
 echo "WRITE READS TO ${OUT_R1} AND ${OUT_R2}"
 
-function runit () {
-    echo "COMMAND"
-    echo "${CMD} > $OUTFILE"
-    date
-    ${CMD} > ${OUTFILE}
-    echo -n $?;    echo " exit status"
-    date
-}
-
 # This filter requires mapQ>=5 on both reads.
 # For a bowtie2 mapping, hardly any reads pass.
 # This assumes sorted inputs only writes the second.
@@ -52,17 +43,13 @@ function runit () {
 # For subtraction, we found that any filter is too restrictive.
 
 echo "SAMTOOLS TO EXTRACT MAPPED READ IDs..."
-# The code below complains the bam must be indexed, but only if run by expanding CMD.
-###CMD="${SAMTOOLS} view ${IN_BAM} | cut -f 1 "
-###OUTFILE="${OUT_PREFIX}.${IN_BAM}.ids"
-###runit
-#
-# The invocation below is a work-around.
 TMP_IDS_FILE="tmp.${IN_BAM1}.${IN_BAM2}.ids"
 echo BAM1   # start a new file with R1 IDs
 ${SAMTOOLS} view ${IN_BAM1} | cut -f 1 >  ${TMP_IDS_FILE}
+echo -n $?; echo " exit status"
 echo BAM2   # append to same file with R2 IDs
 ${SAMTOOLS} view ${IN_BAM1} | cut -f 1 >> ${TMP_IDS_FILE}
+echo -n $?; echo " exit status"
 
 # This script assumes R1 and R2 of a pair have the same ID.
 # If either R1 or R2 mapped, then the ID is saved, and both reads will get subtracted.
@@ -70,10 +57,10 @@ ${SAMTOOLS} view ${IN_BAM1} | cut -f 1 >> ${TMP_IDS_FILE}
 # user must strip the suffixes from the deflines.
 
 EXCLUDE="-v"  # option to exclude the named reads
-CMD="cat ${IN_R1} | ${DIR}/${FILTER} ${EXCLUDE} ${TMP_IDS_FILE} "
-OUTFILE="${OUT_R1}"
-runit
-CMD="cat ${IN_R2} | ${DIR}/${FILTER} ${EXCLUDE} ${TMP_IDS_FILE} "
-OUTFILE="${OUT_R2}"
-runit
+echo MAKE ${OUT_R1}
+${DIR}/${FILTER} ${EXCLUDE} ${TMP_IDS_FILE} < ${IN_R1} > ${OUT_R1}
+echo -n $?; echo " exit status"
+echo MAKE ${OUT_R2}
+${DIR}/${FILTER} ${EXCLUDE} ${TMP_IDS_FILE} < ${IN_R2} > ${OUT_R2}
+echo -n $?; echo " exit status"
 echo DONE
